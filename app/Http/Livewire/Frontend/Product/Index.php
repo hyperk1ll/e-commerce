60ea0081d3e925459479885d39c54a3afcce04e1
Hilmy Tsany <hilmy.tsani@gmail.com>
2023-02-01 03:18:28 +0700

    set currency to rupiah on product

--- app/Http/Livewire/Frontend/Product/Index.php
+++ app/Http/Livewire/Frontend/Product/Index.php
@@ -11,7 +11,7 @@ class Index extends Component
     public $products, $category, $brandInputs = [];
 
     protected $queryString = [
-        'brandInputs' => ['except' => '', 'as' => 'brand' ],
+        'brandInputs' => ['except' => '', 'as' => 'brand'],
     ];
 
     public function mount($category)
@@ -21,12 +21,7 @@ class Index extends Component
 
     public function render()
     {
-        $this->products = Product::where('category_id', $this->category->id)
-                            ->when($this->brandInputs, function ($q) {
-                                $q->whereIn('brand', $this->brandInputs);
-                            })
-                            ->where('status','0')
-                            ->get();
+        $this->products = Product::where('category_id', $this->category->id)->when($this->brandInputs, function ($q) { $q->whereIn('brand', $this->brandInputs);})->where('status','0')->get();
         
         return view('livewire.frontend.product.index', [
             'products' => $this->products,
