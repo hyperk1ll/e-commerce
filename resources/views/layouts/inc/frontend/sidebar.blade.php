d50b94bfe32e778b5260ec2ef451184ad2f9cc65
Aeoline <raubrey.kh@gmail.com>
2023-02-01 13:52:42 +0700

    Footer
    ini dikasi footer tapi belum rapih

---
+++ resources/views/layouts/inc/frontend/sidebar.blade.php
@@ -0,0 +1,24 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link" href="{{url ('/collections')}}" aria-expanded="false" aria-controls="ui-category">
+        <i class="mdi mdi-circle-outline menu-icon"></i>
+        <span class="menu-title">Category</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="index.html">
+        <i class="mdi mdi-home menu-icon"></i>
+        <span class="menu-title">Brands</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="{{url ('collections/products')}}">
+        <i class="mdi mdi-view-headline menu-icon"></i>
+        <span class="menu-title">Products</span>
+      </a>
+    </li>
+    
+  </ul>
+</nav>
